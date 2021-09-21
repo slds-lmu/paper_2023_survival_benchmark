1d7d8f762d3dd21bce411e89fa6672945672fa5c
Lukas Burk <lukas@quantenbrot.de>
2021-09-21 16:42:10 +0200

    Next batch

--- import_new_data.R
+++ import_new_data.R
@@ -62,6 +62,44 @@ aids.id <- JM::aids.id %>%
 saveRDS(aids.id, here::here("code/data_candidates/aids.id.rds"))
 
 
+# joineR::heart.valve -----------------------------------------------------
+
+# Per AB: long format with most features collected at follow up.
+# Could be used if we only keep baseline covariates (which then are only few however).
+# For time-to-event and status variables use last obs. of each subject (num)
+
+# Making sure to arrange by num/time for filtering
+heart.valve <- joineR::heart.valve %>%
+  arrange(num, time)
+
+# time to event: last obs per subject
+heart.valve_tte <- heart.valve %>%
+  select(num, time, status) %>%
+  group_by(num) %>%
+  slice_tail(n = 1) %>%
+  ungroup()
+
+# Covariates: baseline, e.g. first per subject
+heart.valve_covars <- heart.valve %>%
+  select(-time, -status) %>%
+  group_by(num) %>%
+  slice_head(n = 1) %>%
+  ungroup()
+
+# Double check no subject num was lost for whatever reason
+all(unique(heart.valve_covars$num) == unique(heart.valve_tte$num))
+nrow(heart.valve_covars) == nrow(heart.valve_tte)
+
+# Join by num explicitly just in case
+heart.valve <- left_join(
+  heart.valve_tte,
+  heart.valve_covars,
+  by = "num") %>%
+  select(-num)
+
+saveRDS(heart.valve, here::here("code/data_candidates/heart.valve.rds"))
+
+
 # joineR::liver -----------------------------------------------------------
 # Liver cirrhosis drug trial data
 
@@ -70,9 +108,7 @@ saveRDS(aids.id, here::here("code/data_candidates/aids.id.rds"))
 # function was measured (feature prothrombin). Keep the value at time = 0.
 
 liver <- joineR::liver %>%
-  group_by(id) %>%
-  slice_min(time) %>% # First measurement per subject
-  ungroup() %>%
+  filter(time == 0) %>%
   select(
     -id,  # ID var
     -time # Now irrelevant after filtering
@@ -80,6 +116,18 @@ liver <- joineR::liver %>%
   rename(time = survival, status = cens)
 
 saveRDS(liver, here::here("code/data_candidates/liver.rds"))
+
+
+# locfit::livmet ----------------------------------------------------------
+# Survival times for 622 patients diagnosed with Liver Metastases.
+# Beware, the censoring variable is coded as 1 = uncensored, so use cens=1-z in locfit() calls.
+
+livmet <- mlr3misc::load_dataset("livmet", "locfit") %>%
+  rename(time = t, status = z) %>%
+  mutate(status = 1 - status)
+
+saveRDS(livmet, here::here("code/data_candidates/livmet.rds"))
+
 
 # MRsurv::FTR.data --------------------------------------------------------
 # Data were extracted from the DIVAT cohort. It corresponds to the reference
@@ -117,6 +165,26 @@ insem <- mlr3misc::load_dataset("insem", "parfm") %>%
 
 saveRDS(insem, here::here("code/data_candidates/insem.rds"))
 
+# parfm::reconstitution ---------------------------------------------------
+# Reconstitution of blood–milk barrier after reconstitution
+
+reconstitution <- mlr3misc::load_dataset("reconstitution", "parfm") %>%
+  rename(time = Time, status = Status) %>%
+  select(-Cowid) # ID var
+
+saveRDS(reconstitution, here::here("code/data_candidates/reconstitution.rds"))
+
+
+# pec::cost ---------------------------------------------------------------
+# This data set contains a subset of the data from the Copenhagen stroke study
+
+# This seems too god to be true almost, no renaming needed
+cost <- mlr3misc::load_dataset("cost", "pec")
+
+# Check to see if I missed a constant variable maybe?
+all(dim(cost) == dim(janitor::remove_constant(cost)))
+
+saveRDS(cost, here::here("code/data_candidates/cost.rds"))
 
 # relsurv::colrec --------------------------------------------------------------
 # Survival of patients with colon and rectal cancer diagnosed in 1994-2000.
