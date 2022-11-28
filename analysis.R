4407223796ea67e67aefa724e3d3c45f7ad14d33
Lukas Burk <burk@leibniz-bips.de>
2022-11-28 12:45:06 +0100

    Pretest viz msr adjustment

--- analysis.R
+++ analysis.R
@@ -30,11 +30,29 @@ showLog(ids[1])
 ###################################################################################################
 ### Reduce results
 ###################################################################################################
-measures = msrs(c("surv.cindex", "surv.brier"))
+measures = msrs(c("surv.cindex", "surv.rcll"))
 bmr = reduceResultsBatchmark()
+saveRDS(bmr, "tmp/bmr.rds")
+
 profvis::profvis({
 aggr = bmr$aggregate(measures = measures[2], conditions = TRUE)
 })
+saveRDS(aggr, "tmp/aggr.rds")
 resamplings_with_error = aggr[errors > 0, nr]
 mlr3viz::autoplot(bmr)
 # bmr$resample_result(resamplings_with_error[1])$errors
+
+library(ggplot2)
+ggplot(aggr, aes(x = learner_id, y = surv.rcll)) +
+  facet_wrap(vars(task_id)) +
+  geom_boxplot() +
+  scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
+  theme_minimal()
+
+scores = bmr$score(msrs(c("surv.rcll")))
+
+ggplot(scores, aes(x = learner_id, y = surv.rcll)) +
+  facet_wrap(vars(task_id)) +
+  geom_boxplot() +
+  scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
+  theme_minimal()
