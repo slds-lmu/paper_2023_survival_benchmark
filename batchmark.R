8925ca6cc86e4e63ddab94cdc59a446628f46135
Lukas Burk <burk@leibniz-bips.de>
2023-10-02 17:37:24 +0200

    Disable DLs, tweak preproc

--- batchmark.R
+++ batchmark.R
@@ -1,9 +1,8 @@
 root = here::here()
 source(file.path(root, "settings.R"))
+#source(file.path(root, "settings_debug.R"))
 
-###################################################################################################
-### Packages ----
-###################################################################################################
+# Packages ----------------------------------------------------------------
 
 # Dependencies managed via renv. Manually update as necessary via renv::update()
 # See also attic/_dependencies.R
@@ -66,6 +65,7 @@ bl = function(key, ..., .encode = FALSE, .scale = FALSE) { # get base learner wi
   # Added form as per RS
   g = ppl("distrcompositor", learner = learner, form = 'ph')
 
+  # scaling only used for SSVM + deep learners
   if (.scale) {
     g = po("scale") %>>% g
   }
@@ -74,7 +74,12 @@ bl = function(key, ..., .encode = FALSE, .scale = FALSE) { # get base learner wi
     g = po("encode", method = "treatment") %>>% g
   }
 
-  as_learner(po("fixfactors") %>>% po("collapsefactors", target_level_count = 5) %>>% g)
+  po("fixfactors") %>>%
+    # po("collapsefactors", no_collapse_above_prevalence = 0.01) # not sure which is more appropriate
+    po("collapsefactors", target_level_count = 5) %>>%
+    g %>>%
+    po("removeconstants") |>
+    as_learner()
 }
 
 auto_tune = function(learner, ...) { # wrap into random search
@@ -83,11 +88,11 @@ auto_tune = function(learner, ...) { # wrap into random search
   if (is.null(search_space$trafo))
     checkmate::assert_subset(names(search_space$params), names(learner$param_set$params))
 
-  at = AutoTuner$new(
+  AutoTuner$new(
     learner = learner,
     search_space = search_space,
     resampling = rsmp("cv", folds = inner_folds),
-    measure = measure,
+    measure = measure, # Measure will be set via global variable in loop
     terminator = trm("evals", n_evals = budget_constant, k = budget_multiplier),
     tuner = tnr("random_search"),
     store_tuning_instance = TRUE,
@@ -118,6 +123,8 @@ for (measure in measures) {
 
     ,
 
+    # survivalmodels::akritas
+    # https://raphaels1.github.io/survivalmodels/reference/akritas.html
     AF = auto_tune(
       bl("surv.akritas"),
       surv.akritas.lambda = p_dbl(0, 1)
@@ -196,6 +203,7 @@ for (measure in measures) {
       bl("surv.aorsf", n_tree = 5000, control_type = "fast"),
       surv.aorsf.mtry_ratio = p_dbl(0, 1),
       surv.aorsf.leaf_min_events = p_int(5, 50),
+      # alpha only tunable when control_type == "net" (uses glmnet)
       # surv.aorsf.control_net_alpha = p_dbl(0, 1),
       .extra_trafo = function(x, param_set) {
         x$surv.aorsf.split_min_obs = x$surv.aorsf.leaf_min_events + 5L
@@ -250,94 +258,94 @@ for (measure in measures) {
       }
     )
 
-    ,
-
-    CoxT = auto_tune(
-      bl("surv.coxtime", standardize_time = TRUE, epochs = 100, optimizer = "adam", .encode = TRUE, .scale = TRUE),
-      surv.coxtime.dropout = p_dbl(0, 1),
-      surv.coxtime.weight_decay = p_dbl(0, 0.5),
-      surv.coxtime.learning_rate = p_dbl(0, 1),
-      surv.coxtime.nodes = p_int(1, 32),
-      surv.coxtime.k = p_int(1, 4),
-      .extra_trafo = function(x, param_set) {
-        x$surv.coxtime.num_nodes = rep(x$surv.coxtime.nodes, x$surv.coxtime.k)
-        x$surv.coxtime.nodes = x$surv.coxtime.k = NULL
-        x
-      }
-    )
-
-    ,
-
-    DH = auto_tune(
-      bl("surv.deephit", optimizer = "adam", .encode = TRUE, .scale = TRUE),
-      surv.deephit.nodes = p_int(1, 32),
-      surv.deephit.k = p_int(1, 4),
-      surv.deephit.dropout = p_dbl(0, 1),
-      surv.deephit.weight_decay = p_dbl(0, 5),
-      surv.deephit.learning_rate = p_dbl(0, 1),
-      .extra_trafo = function(x, param_set) {
-        x$surv.deephit.num_nodes = rep(x$surv.deephit.nodes, x$surv.deephit.k)
-        x$surv.deephit.nodes = x$surv.deephit.k = NULL
-        x
-      }
-    )
-
-    ,
-
-    DS = auto_tune(
-      bl("surv.deepsurv", optimizer = "adam", .encode = TRUE, .scale = TRUE),
-      surv.deepsurv.nodes = p_int(1, 32),
-      surv.deepsurv.k = p_int(1, 4),
-      surv.deepsurv.dropout = p_dbl(0, 1),
-      surv.deepsurv.weight_decay = p_dbl(0, 5),
-      surv.deepsurv.learning_rate = p_dbl(0, 1),
-      .extra_trafo = function(x, param_set) {
-        x$surv.deepsurv.num_nodes = rep(x$surv.deepsurv.nodes, x$surv.deepsurv.k)
-        x$surv.deepsurv.nodes = x$surv.deepsurv.k = NULL
-        x
-      }
-    )
-
-    ,
-
-    LH = auto_tune(
-      bl("surv.loghaz", optimizer = "adam", .encode = TRUE, .scale = TRUE),
-      surv.loghaz.nodes = p_int(1, 32),
-      surv.loghaz.k = p_int(1, 4),
-      surv.loghaz.dropout = p_dbl(0, 1),
-      surv.loghaz.weight_decay = p_dbl(0, 5),
-      surv.loghaz.learning_rate = p_dbl(0, 1),
-      .extra_trafo = function(x, param_set) {
-        x$surv.loghaz.num_nodes = rep(x$surv.loghaz.nodes, x$surv.loghaz.k)
-        x$surv.loghaz.nodes = x$surv.loghaz.k = NULL
-        x
-      }
-    )
-
-    ,
-
-    PCH = auto_tune(
-      bl("surv.pchazard", optimizer = "adam", .encode = TRUE, .scale = TRUE),
-      surv.pchazard.nodes = p_int(1, 32),
-      surv.pchazard.k = p_int(1, 4),
-      surv.pchazard.dropout = p_dbl(0, 1),
-      surv.pchazard.weight_decay = p_dbl(0, 5),
-      surv.pchazard.learning_rate = p_dbl(0, 1),
-      .extra_trafo = function(x, param_set) {
-        x$surv.pchazard.num_nodes = rep(x$surv.pchazard.nodes, x$surv.pchazard.k)
-        x$surv.pchazard.nodes = x$surv.pchazard.k = NULL
-        x
-      }
-    )
-
-    ,
-
-    DNN = auto_tune(
-      bl("surv.dnnsurv", optimizer = "adam", epochs = 100, .encode = TRUE, .scale = TRUE),
-      surv.dnnsurv.decay = p_dbl(0, 0.5),
-      surv.dnnsurv.lr = p_dbl(0, 1),
-      surv.dnnsurv.cuts = p_int(3, 50)
-    )
+#    ,
+#
+#    CoxT = auto_tune(
+#      bl("surv.coxtime", standardize_time = TRUE, epochs = 100, optimizer = "adam", .encode = TRUE, .scale = TRUE),
+#      surv.coxtime.dropout = p_dbl(0, 1),
+#      surv.coxtime.weight_decay = p_dbl(0, 0.5),
+#      surv.coxtime.learning_rate = p_dbl(0, 1),
+#      surv.coxtime.nodes = p_int(1, 32),
+#      surv.coxtime.k = p_int(1, 4),
+#      .extra_trafo = function(x, param_set) {
+#        x$surv.coxtime.num_nodes = rep(x$surv.coxtime.nodes, x$surv.coxtime.k)
+#        x$surv.coxtime.nodes = x$surv.coxtime.k = NULL
+#        x
+#      }
+#    )
+#
+#    ,
+#
+#     DH = auto_tune(
+#       bl("surv.deephit", optimizer = "adam", .encode = TRUE, .scale = TRUE),
+#       surv.deephit.nodes = p_int(1, 32),
+#       surv.deephit.k = p_int(1, 4),
+#       surv.deephit.dropout = p_dbl(0, 1),
+#       surv.deephit.weight_decay = p_dbl(0, 5),
+#       surv.deephit.learning_rate = p_dbl(0, 1),
+#       .extra_trafo = function(x, param_set) {
+#         x$surv.deephit.num_nodes = rep(x$surv.deephit.nodes, x$surv.deephit.k)
+#         x$surv.deephit.nodes = x$surv.deephit.k = NULL
+#         x
+#       }
+#     )
+#
+#     ,
+#
+#     DS = auto_tune(
+#       bl("surv.deepsurv", optimizer = "adam", .encode = TRUE, .scale = TRUE),
+#       surv.deepsurv.nodes = p_int(1, 32),
+#       surv.deepsurv.k = p_int(1, 4),
+#       surv.deepsurv.dropout = p_dbl(0, 1),
+#       surv.deepsurv.weight_decay = p_dbl(0, 5),
+#       surv.deepsurv.learning_rate = p_dbl(0, 1),
+#       .extra_trafo = function(x, param_set) {
+#         x$surv.deepsurv.num_nodes = rep(x$surv.deepsurv.nodes, x$surv.deepsurv.k)
+#         x$surv.deepsurv.nodes = x$surv.deepsurv.k = NULL
+#         x
+#       }
+#     )
+#
+#     ,
+#
+#     LH = auto_tune(
+#       bl("surv.loghaz", optimizer = "adam", .encode = TRUE, .scale = TRUE),
+#       surv.loghaz.nodes = p_int(1, 32),
+#       surv.loghaz.k = p_int(1, 4),
+#       surv.loghaz.dropout = p_dbl(0, 1),
+#       surv.loghaz.weight_decay = p_dbl(0, 5),
+#       surv.loghaz.learning_rate = p_dbl(0, 1),
+#       .extra_trafo = function(x, param_set) {
+#         x$surv.loghaz.num_nodes = rep(x$surv.loghaz.nodes, x$surv.loghaz.k)
+#         x$surv.loghaz.nodes = x$surv.loghaz.k = NULL
+#         x
+#       }
+#     )
+#
+#     ,
+#
+#     PCH = auto_tune(
+#       bl("surv.pchazard", optimizer = "adam", .encode = TRUE, .scale = TRUE),
+#       surv.pchazard.nodes = p_int(1, 32),
+#       surv.pchazard.k = p_int(1, 4),
+#       surv.pchazard.dropout = p_dbl(0, 1),
+#       surv.pchazard.weight_decay = p_dbl(0, 5),
+#       surv.pchazard.learning_rate = p_dbl(0, 1),
+#       .extra_trafo = function(x, param_set) {
+#         x$surv.pchazard.num_nodes = rep(x$surv.pchazard.nodes, x$surv.pchazard.k)
+#         x$surv.pchazard.nodes = x$surv.pchazard.k = NULL
+#         x
+#       }
+#     )
+#
+#     ,
+#
+#     DNN = auto_tune(
+#       bl("surv.dnnsurv", optimizer = "adam", epochs = 100, .encode = TRUE, .scale = TRUE),
+#       surv.dnnsurv.decay = p_dbl(0, 0.5),
+#       surv.dnnsurv.lr = p_dbl(0, 1),
+#       surv.dnnsurv.cuts = p_int(3, 50)
+#     )
   )
 
   imap(learners, function(l, id) l$id = id)
@@ -358,19 +366,20 @@ for (measure in measures) {
 
 summarizeExperiments(by = c("task_id", "learner_id"))
 
+alljobs = unnest(getJobTable(), c("prob.pars", "algo.pars"))[, .(job.id, repl, tags, task_id, learner_id)]
+data.table::setnames(alljobs, "tags", "measure")
 
-###################################################################################################
-### Pretest
-###################################################################################################
-res = list(walltime = 4 * 3600, memory = 4096)
-ids = findExperiments(repls = 1)
-ids = ijoin(ids, findTagged("rcll"))
+# Pretest -----------------------------------------------------------------
+if (FALSE) {
+  res = list(walltime = 4 * 3600, memory = 4096)
+  ids = findExperiments(repls = 1)
+  ids = ijoin(ids, findTagged("rcll"))
 
-# submitJobs(ids, resources = res)
+  submitJobs(ids, resources = res)
+}
 
-###################################################################################################
-### Submit
-###################################################################################################
+
+# Submit ------------------------------------------------------------------
 
 if (FALSE) {
   tab = unnest(getJobTable(), c("prob.pars", "algo.pars"))
