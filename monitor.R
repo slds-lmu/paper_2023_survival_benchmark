41b0a983d23bb00b9c608746ed836a175c598bc6
Lukas Burk <burk@leibniz-bips.de>
2023-10-09 08:52:06 +0200

    Trim monitoring output

--- monitor.R
+++ monitor.R
@@ -19,6 +19,8 @@ if (nrow(tbl_running) > 0) {
   cli::cli_h2("Running: {reg_dir}")
   tbl_running[, ..job_vars]
   print(tbl_running[, .(count = .N), by = "learner_id"])
+  cat("\n")
+  print(tbl_running[, .(count = .N), by = "task_id"])
 }
 
 # Done --------------------------------------------------------------------
@@ -27,6 +29,8 @@ if (nrow(tbl_done) > 0) {
   cli::cli_h2("Done: {reg_dir}")
   tbl_done <- tbl_done[, ..job_vars]
   print(tbl_done[, .(count = .N), by = "learner_id"])
+  cat("\n")
+  print(tbl_done[, .(count = .N), by = "task_id"])
 }
 
 cat("\n")
@@ -37,6 +41,7 @@ if (nrow(tbl_expired) > 0) {
   cli::cli_h2("Expired: {reg_dir}")
   tbl_expired <- tbl_expired[, ..job_vars]
   print(tbl_expired[, .(count = .N), by = "learner_id"])
+  print(tbl_expired[, .(count = .N), by = "task_id"])
 }
 
 cat("\n")
@@ -47,8 +52,20 @@ if (nrow(tbl_errors) > 0) {
   cli::cli_h2("Errors: {reg_dir}")
   tbl_errors <- tbl_errors[, ..job_vars]
   print(tbl_errors[, .(count = .N), by = "learner_id"])
+  cat("\n")
+  print(tbl_errors[, .(count = .N), by = "task_id"])
 
-  print(getErrorMessages(findErrors(reg = reg), reg = reg)[, "message"][, .(count = .N, message = stringr::str_squish(message)), by = "message"])
+  errmsg <- getErrorMessages(findErrors(reg = reg), reg = reg)[, "message"]
+  errmsg <- errmsg[, .(count = .N), by = "message"]
+  errmsg$message <- stringr::str_remove(errmsg$message, "Error : Task ")
+  errmsg$message <- stringr::str_remove(errmsg$message, ", but learner")
+  errmsg$message <- stringr::str_replace(errmsg$message, "has missing values in column\\(s\\)", "miss:")
+  errmsg$message <- stringr::str_replace_all(errmsg$message, "'Center\\.", "C")
+  errmsg$message <- stringr::str_remove(errmsg$message, "surv\\.\\w+'s ")
+  errmsg$message <- stringr::str_remove(errmsg$message, "does not support this\\nThis happened PipeOp ")
+  errmsg$message <- stringr::str_trunc(errmsg$message, 90)
+
+  print(errmsg)
 
 }
 
