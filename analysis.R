13fa07596f2b59f614c7544704ff4f87a7d9527a
Lukas Burk <lukas@quantenbrot.de>
2024-01-16 16:21:53 +0100

    Cleanup

--- analysis.R
+++ analysis.R
@@ -13,13 +13,15 @@ settings = config::get()
 library(batchtools)
 library(mlr3batchmark)
 library(mlr3benchmark)
+library(mlr3proba)
+requireNamespace("mlr3extralearners")
 # requires PMCMRplus, not included in renv because of issues installing it on cluster (libmpfr.so.6)
 library(ggplot2)
 
 reg_dir = file.path(root, settings$reg_name)
-reg = loadRegistry(reg_dir, writeable = TRUE)
+# reg = loadRegistry(reg_dir, writeable = TRUE)
 
-alljobs = collect_job_table(reg)
+# alljobs = collect_job_table(reg)
 result_path = here::here("results")
 
 # Reassembling tuning archives ----------------------------------------------------------------
@@ -34,14 +36,19 @@ if (FALSE) {
 # Store eval measures for easier retrieval
 measures_eval = get_measures_eval()
 
-collect_results(reg, tuning_measure = "harrell_c", measures_eval = measures_eval, result_path = here::here("results"))
-collect_results(reg, tuning_measure = "rcll", measures_eval = measures_eval, result_path = here::here("results"))
+collect_results(settings$reg_name, tuning_measure = "harrell_c", measures_eval = measures_eval, result_path = here::here("results"))
+collect_results(settings$reg_name, tuning_measure = "rcll", measures_eval = measures_eval, result_path = here::here("results"))
 
+bmr_harrell_c  = readRDS(fs::path(result_path, settings$reg_name, "bmr_harrell_c.rds"))
 aggr_harrell_c = readRDS(fs::path(result_path, settings$reg_name, "aggr_harrell_c.rds"))
-bmr_harrell_c = readRDS(fs::path(result_path, settings$reg_name, "aggr_harrell_c.rds"))
-bma_harrell_c = readRDS(fs::path(result_path, settings$reg_name, "bma_harrell_c.rds"))
+bma_harrell_c  = readRDS(fs::path(result_path, settings$reg_name, "bma_harrell_c.rds"))
 
 # Quick check ---------------------------------------------------------------------------------
+
+bmr_tab = bmr_harrell_c$aggregate(measures = list(), conditions = TRUE)
+bmr_tab[errors > 0,][, .(n = .N), by = .(task_id)]
+bmr_tab[errors > 0,][, .(n = .N), by = .(learner_id)]
+
 
 bma_harrell_c$friedman_posthoc(meas = measures_eval$harrell_c$id)
 
