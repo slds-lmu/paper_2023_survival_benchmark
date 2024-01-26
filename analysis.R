0236ea84805a8882ead28548452e307a8036bea4
Lukas Burk <lukas@quantenbrot.de>
2024-01-26 17:18:05 +0100

    Cleanup

--- analysis.R
+++ analysis.R
@@ -19,9 +19,9 @@ requireNamespace("mlr3extralearners")
 library(ggplot2)
 
 reg_dir = file.path(root, settings$reg_name)
-# reg = loadRegistry(reg_dir, writeable = TRUE)
+reg = loadRegistry(reg_dir, writeable = TRUE)
 
-# alljobs = collect_job_table(reg)
+alljobs = collect_job_table(reg)
 result_path = here::here("results")
 
 # Reassembling tuning archives ----------------------------------------------------------------
@@ -34,21 +34,43 @@ if (FALSE) {
 # Reducing results ----------------------------------------------------------------------------
 
 # Store eval measures for easier retrieval
-measures_eval = get_measures_eval()
-measures_eval_ids = mlr3misc::ids(measures_eval)
+msr_tbl = measures_tbl()
+measures_eval = msr_tbl$measure
+measures_eval_ids = msr_tbl$id
 
-collect_results(settings$reg_name, tuning_measure = "harrell_c", measures_eval = measures_eval, result_path = here::here("results"))
-collect_results(settings$reg_name, tuning_measure = "rcll", measures_eval = measures_eval, result_path = here::here("results"))
+# Task ids where all jobs are done
+done_tasks = check_job_state(byvars = c("task_id"))[expired == "—"]$task_id
+done_task_ids = alljobs[task_id %in% done_tasks]
+
+# Creating bmr and bma for jobs tuned with harrell's c and untuned/coxboost, saving to result_path
+collect_results(
+  reg_name = settings$reg_name,
+  id_filter = done_task_ids,
+  tuning_measure = "harrell_c",
+  measures_eval = measures_eval,
+  result_path = here::here("results"),
+)
+
+# Same for RCLL
+collect_results(
+  reg_name = settings$reg_name,
+  id_filter = done_task_ids,
+  tuning_measure = "rcll",
+  measures_eval = measures_eval,
+  result_path = here::here("results"),
+)
+
+# Restoring from disk for further processing
+settings$reg_name = "registry_beartooth_completed_tasks"
 
 bmr_harrell_c  = readRDS(fs::path(result_path, settings$reg_name, "bmr_harrell_c.rds"))
-# aggr_harrell_c = readRDS(fs::path(result_path, settings$reg_name, "aggr_harrell_c.rds"))
+bmrtab_harrell_c  = readRDS(fs::path(result_path, settings$reg_name, "bmrtab_harrell_c.rds"))
 bma_harrell_c  = readRDS(fs::path(result_path, settings$reg_name, "bma_harrell_c.rds"))
 
 # Quick check ---------------------------------------------------------------------------------
 
-bmr_tab = bmr_harrell_c$aggregate(measures = list(), conditions = TRUE)
-bmr_tab[errors > 0,][, .(n = .N), by = .(task_id)]
-bmr_tab[errors > 0,][, .(n = .N), by = .(learner_id)]
+bmrtab_harrell_c[errors > 0,][, .(n = .N), by = .(task_id)]
+bmrtab_harrell_c[errors > 0,][, .(n = .N), by = .(learner_id)]
 
 bma_harrell_c$friedman_posthoc(meas = measures_eval$harrell_c$id)
 
@@ -58,8 +80,8 @@ mlr3viz::autoplot(bma_harrell_c, type = "box", meas = measures_eval$harrell_c$id
 mlr3viz::autoplot(bma_harrell_c, type = "fn", meas = measures_eval$harrell_c$id)
 
 mlr3viz::autoplot(bma_harrell_c, type = "cd", meas = measures_eval$harrell_c$id, test = "nemenyi")
+mlr3viz::autoplot(bma_harrell_c, type = "cd", meas = measures_eval$harrell_c$id, test = "nemenyi", ratio = 1/3)
 mlr3viz::autoplot(bma_harrell_c, type = "cd", meas = measures_eval$harrell_c$id, test = "bd", baseline = "CPH")
-
 
 # Bulk-write all relevant plots ---------------------------------------------------------------
 
@@ -69,13 +91,22 @@ if (!fs::dir_exists(fs::path(result_path, settings$reg_name, "harrell_c"))) {
 
 for (measure_id in measures_eval_ids) {
   for (type in c("mean", "box", "fn", "cd")) {
+
+    if (msr_tbl[id == measure_id, erv] & type %in% c("fn", "cd")) next
+
+
     file_path = fs::path(result_path, settings$reg_name, "harrell_c", glue::glue("{measure_id}_{type}"))
     print(file_path)
 
     try({
-      p = mlr3viz::autoplot(bma_harrell_c, type = type, meas = measure_id) +
+      p = mlr3viz::autoplot(
+        bma_harrell_c,
+        type = type,
+        meas = measure_id,
+        minimize = msr_tbl[id == measure_id, minimize]
+      ) +
         labs(
-          x = "Learner",
+          x = NULL,
           y = measures_eval[[measure_id]]$label,
           caption = glue::glue("Tuning measure: Harrell's C")
         ) +
@@ -86,5 +117,4 @@ for (measure_id in measures_eval_ids) {
     })
 
   }
-
 }
