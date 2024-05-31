2fe0594c41d214e216df6ff7b95171a4dc414523
Lukas Burk <lukas@quantenbrot.de>
2024-05-31 18:18:13 +0200

    Prep result site expansion

---
+++ site/common.R
@@ -0,0 +1,33 @@
+knitr::opts_chunk$set(message = FALSE, warning = FALSE, echo = FALSE, dev = "ragg_png")
+
+source(here::here("helpers.R"))
+
+settings = config::get(config = "beartooth")
+reg_dir = here::here(settings$reg_name)
+result_path = here::here("results")
+
+###################################################################################################
+### Packages
+###################################################################################################
+library(mlr3benchmark)
+library(mlr3proba)
+requireNamespace("mlr3extralearners")
+# requires PMCMRplus, not included in renv because of issues installing it on cluster (libmpfr.so.6)
+library(ggplot2)
+library(kableExtra)
+library(dplyr)
+
+result_path = here::here("results", settings$reg_name)
+msr_tbl = measures_tbl()
+measures_eval_ids = msr_tbl$id
+
+lrntab = load_lrntab()
+tasktab = load_tasktab()
+
+bma_harrell_c = readRDS(fs::path(result_path, "bma_clean_harrell_c.rds"))
+bma_rcll      = readRDS(fs::path(result_path, "bma_clean_rcll.rds"))
+
+bmrtab_harrell_c = readRDS(fs::path(result_path, "bmrtab_harrell_c.rds"))
+bmrtab_rcll = readRDS(fs::path(result_path, "bmrtab_rcll.rds"))
+
+bma = readRDS(fs::path(result_path, "bma_full.rds"))
