737ea3c887e40c6c893a3d6707018f00a6fe72a7
Lukas Burk <burk@leibniz-bips.de>
2025-02-13 16:51:50 +0000

    Adjust preprocessing for wa_churn

--- import_datasets.R
+++ import_datasets.R
@@ -4,24 +4,25 @@ library(dplyr)
 
 save_data = function(x, path = here::here("datasets/")) {
   xname = deparse(substitute(x))
-  path_file_rds = paste0(path, xname, ".rds")
-  path_file_csv = paste0(path, xname, ".csv")
-  message("Saving ", xname, " at ", path_file_rds, " and ", path_file_csv)
-
+  path_file_rds = fs::path(path, xname, ext = "rds")
+  path_file_csv = fs::path(path, xname, ext = "csv")
+  cli::cli_alert_info("Saving {xname} at {.file {fs::path_rel(path_file_rds)}} and {.file {fs::path_rel(path_file_csv)}}")
+  
   checkmate::assert_integerish(x[["status"]], lower = 0, upper = 1)
-  checkmate::assert_numeric(x[["time"]], lower = 0)
-
+  
   x_nomiss = na.omit(x)
-
+  
   if (nrow(x) - nrow(x_nomiss) > 0) {
-    message("Dropping ", nrow(x) - nrow(x_nomiss), " rows due to missings")
+    cli::cli_alert_warning("Dropping {.val {nrow(x) - nrow(x_nomiss)}} rows due to missings")
   }
-
+  
   time_zeros = which(x_nomiss$time == 0)
   if (length(time_zeros) > 0) {
-    message("Dropping ", length(time_zeros), " rows due to time = 0")
+    cli::cli_alert_warning("Dropping {.val length(time_zeros)} rows due to {.val time} equaling 0")
     x_nomiss = x_nomiss[-time_zeros, ]
   }
+  
+  checkmate::assert_numeric(x_nomiss[["time"]], lower = .Machine$double.xmin)
 
   saveRDS(x_nomiss, file = path_file_rds)
   write.csv(x_nomiss, file = path_file_csv, row.names = FALSE)
@@ -31,18 +32,18 @@ save_data = function(x, path = here::here("datasets/")) {
 # dynpred::ova --------------------------------------------------------------------------------
 
 ova = mlr3misc::load_dataset("ova", "dynpred") |>
-  mutate(
-    time = tyears,
-    status = as.integer(d),
-    Broders = factor(Broders),
-    FIGOIII = as.integer(FIGO == "III"),
-    Ascites = factor(Ascites),
-    # Remove > < symbols as they can cause issues
-    Diam = stringr::str_replace(Diam, ">", "gt"),
-    Diam = stringr::str_replace(Diam, "<", "lt"),
-    Diam = factor(Diam)
-  ) |>
-  select(-tyears, -d, -FIGO, -id)
+mutate(
+  time = tyears,
+  status = as.integer(d),
+  Broders = factor(Broders),
+  FIGOIII = as.integer(FIGO == "III"),
+  Ascites = factor(Ascites),
+  # Remove > < symbols as they can cause issues
+  Diam = stringr::str_replace(Diam, ">", "gt"),
+  Diam = stringr::str_replace(Diam, "<", "lt"),
+  Diam = factor(Diam)
+) |>
+select(-tyears, -d, -FIGO, -id)
 
 save_data(ova)
 
@@ -50,11 +51,11 @@ save_data(ova)
 # dynpred::wbc1 -------------------------------------------------------------------------------
 
 wbc1 = mlr3misc::load_dataset("wbc1", "dynpred") |>
-  mutate(
-    time = as.numeric(tyears),
-    status = as.integer(d)
-  ) %>%
-  select(-tyears, -d, -patnr)
+mutate(
+  time = as.numeric(tyears),
+  status = as.integer(d)
+) %>%
+select(-tyears, -d, -patnr)
 
 save_data(wbc1)
 
@@ -64,12 +65,12 @@ save_data(wbc1)
 # or until death or out-migration
 
 child = eha::child |>
-  select(
-    -enter, # = 0 for all obs
-    -id, -m.id, # ID vars
-    -birthdate, # YYYY-MM-DD variable, reasonable to exclude?
-    time = exit, status = event
-  )
+select(
+  -enter, # = 0 for all obs
+  -id, -m.id, # ID vars
+  -birthdate, # YYYY-MM-DD variable, reasonable to exclude?
+  time = exit, status = event
+)
 
 save_data(child)
 
@@ -80,11 +81,11 @@ save_data(child)
 # defined as time from randomization to the date of the first recurrence.
 
 bladder0 = mlr3misc::load_dataset("bladder0", "frailtyHL") |>
-  rename(time = Surtime, status = Status) |>
-  mutate(
-    Center = factor(Center) # Group ID was integer
-    # time = ifelse(time == 0, 0.001, time)
-  )
+rename(time = Surtime, status = Status) |>
+mutate(
+  Center = factor(Center) # Group ID was integer
+  # time = ifelse(time == 0, 0.001, time)
+)
 
 save_data(bladder0)
 
@@ -93,16 +94,16 @@ save_data(bladder0)
 # 52k unique hard drives.
 
 hdfail = frailtySurv::hdfail |>
-  select(-serial) # ID column
+select(-serial) # ID column
 
 summary(hdfail$time)
 
 # Coarsened version to reduce number of unique time points
 # Values 0 < t < 1 will be rounded to time = 0, save_data() drops these
 hdfail = hdfail |>
-  dplyr::mutate(
-    time = floor(time)
-  )
+dplyr::mutate(
+  time = floor(time)
+)
 
 save_data(hdfail)
 
@@ -113,12 +114,12 @@ save_data(hdfail)
 
 # First measurements of JM::aids, therefore obstime == 0 here
 aids.id = JM::aids.id |>
-  rename(time = Time, status = death) |>
-  select(
-    -patient, # ID var
-    -obstime, # 0 for all in this version, first measurement,
-    -start, -stop, -event # not relevant here (per AB)
-  )
+rename(time = Time, status = death) |>
+select(
+  -patient, # ID var
+  -obstime, # 0 for all in this version, first measurement,
+  -start, -stop, -event # not relevant here (per AB)
+)
 
 save_data(aids.id)
 
@@ -172,12 +173,12 @@ save_data(aids.id)
 # function was measured (feature prothrombin). Keep the value at time = 0.
 
 liver = joineR::liver |>
-  filter(time == 0) |>
-  select(
-    -id, # ID var
-    -time # Now irrelevant after filtering
-  ) |>
-  rename(time = survival, status = cens)
+filter(time == 0) |>
+select(
+  -id, # ID var
+  -time # Now irrelevant after filtering
+) |>
+rename(time = survival, status = cens)
 
 save_data(liver)
 
@@ -185,28 +186,28 @@ save_data(liver)
 
 
 channing = mlr3misc::load_dataset("channing", "KMsurv") |>
-  mutate(
-    time = as.numeric(time),
-    status = death,
-    genderF = as.integer(gender - 1),
-    ageentry = as.numeric(ageentry)
-  ) %>%
-  select(-obs, -age, -death, -gender) |>
-  filter(time > 0)
+mutate(
+  time = as.numeric(time),
+  status = death,
+  genderF = as.integer(gender - 1),
+  ageentry = as.numeric(ageentry)
+) %>%
+select(-obs, -age, -death, -gender) |>
+filter(time > 0)
 
 save_data(channing)
 
 # KMsurv::kidtran -----------------------------------------------------------------------------
 
 kidtran = mlr3misc::load_dataset("kidtran", "KMsurv") |>
-  mutate(
-    time = as.numeric(time),
-    status = delta,
-    genderF = as.integer(gender - 1),
-    raceBlack = as.integer(race - 1),
-    age = as.numeric(age)
-  ) |>
-  select(-obs, -delta, -gender, -race)
+mutate(
+  time = as.numeric(time),
+  status = delta,
+  genderF = as.integer(gender - 1),
+  raceBlack = as.integer(race - 1),
+  age = as.numeric(age)
+) |>
+select(-obs, -delta, -gender, -race)
 
 save_data(kidtran)
 
@@ -214,28 +215,28 @@ save_data(kidtran)
 # data from Section 1.12 in Klein and Moeschberger (1997)
 
 std = mlr3misc::load_dataset("std", "KMsurv") |>
-  rename(
-    status = rinfct
-  ) |>
-  mutate(
-    condom = factor(condom, labels = c("always", "sometime", "never")),
-    iinfct = factor(iinfct, labels = c("gonorrhea", "chlamydia", "both"))
-  ) |>
-  select(-obs) # Identifier
+rename(
+  status = rinfct
+) |>
+mutate(
+  condom = factor(condom, labels = c("always", "sometime", "never")),
+  iinfct = factor(iinfct, labels = c("gonorrhea", "chlamydia", "both"))
+) |>
+select(-obs) # Identifier
 
 save_data(std)
 
 # MASS::Aids2 ---------------------------------------------------------------------------------
 
 aids2 = mlr3misc::load_dataset("Aids2", "MASS") %>%
-  mutate(
-    time = as.numeric(death - diag),
-    sexF = if_else(sex == "F", 1L, 0L),
-    status = if_else(status == "D", 1L, 0L),
-    age = as.numeric(age)
-  ) %>%
-  select(-diag, -death, -sex) %>%
-  filter(time > 0)
+mutate(
+  time = as.numeric(death - diag),
+  sexF = if_else(sex == "F", 1L, 0L),
+  status = if_else(status == "D", 1L, 0L),
+  age = as.numeric(age)
+) %>%
+select(-diag, -death, -sex) %>%
+filter(time > 0)
 
 save_data(aids2)
 
@@ -261,7 +262,7 @@ save_data(whas)
 
 if (FALSE) library("modeldata")
 cat_adoption = mlr3misc::load_dataset("cat_adoption", "modeldata") |>
-  rename(status = event)
+rename(status = event)
 
 save_data(cat_adoption)
 
@@ -270,8 +271,8 @@ save_data(cat_adoption)
 # https://modeldata.tidymodels.org/reference/check_times.html
 
 check_times = mlr3misc::load_dataset("check_times", "modeldata") |>
-  select(-package) |> # identifier variable
-  rename(time = check_time)
+select(-package) |> # identifier variable
+rename(time = check_time)
 
 save_data(check_times)
 
@@ -282,7 +283,19 @@ save_data(check_times)
 wa_churn = mlr3misc::load_dataset("wa_churn", "modeldata") |>
   mutate(status = ifelse(churn == "Yes", 1, 0))  |> # No missings, so this suffices
   select(-churn) |>
-  rename(time = tenure)
+  rename(time = tenure) |>
+  mutate(across(where(is.factor), \(x) {
+    # Remove spaces and parantheses from factor levels to avoid downstream issues in surv.parametric
+    x = as.character(x) |> 
+      stringr::str_replace_all(pattern = "\\s", replacement = "_") |>
+      stringr::str_replace_all(pattern = "[\\(\\)]", replacement = "")
+      
+    # Lump factor levels together to avoid singularities in model fit
+    x = if_else(x == "No_internet_service", "No", x)
+    x = if_else(x == "No_phone_service", "No", x)
+    
+    as.factor(x)
+  }))
 
 save_data(wa_churn)
 
@@ -314,10 +327,10 @@ save_data(wa_churn)
 # sample constituted by first transplant recipients (FTR).
 
 dataFTR = mlr3misc::load_dataset("dataFTR", "RISCA") |>
-  rename(
-    time = Tps.Evt,
-    status = Evt
-  )
+rename(
+  time = Tps.Evt,
+  status = Evt
+)
 
 save_data(dataFTR)
 
@@ -327,10 +340,10 @@ save_data(dataFTR)
 # "Tattente2cl" = Waiting time between consecutive transplants, ok to include?
 
 dataSTR = mlr3misc::load_dataset("dataSTR", "RISCA") |>
-  rename(
-    time = Tps.Evt,
-    status = Evt
-  )
+rename(
+  time = Tps.Evt,
+  status = Evt
+)
 
 save_data(dataSTR)
 
@@ -354,18 +367,18 @@ save_data(dataSTR)
 # pammtools::tumor ----------------------------------------------------------------------------
 
 tumor = pammtools::tumor %>%
-  mutate(
-    time = days,
-    transfusion = if_else(transfusion == "yes", 1L, 0L),
-    complications = if_else(complications == "yes", 1L, 0L),
-    metastases = if_else(metastases == "yes", 1L, 0L),
-    resection = if_else(resection == "yes", 1L, 0L),
-    charlson_score = factor(charlson_score),
-    sexF = as.integer(sex == "female"),
-    age = as.numeric(age)
-  ) %>%
-  select(-days, -sex) %>%
-  as.data.frame()
+mutate(
+  time = days,
+  transfusion = if_else(transfusion == "yes", 1L, 0L),
+  complications = if_else(complications == "yes", 1L, 0L),
+  metastases = if_else(metastases == "yes", 1L, 0L),
+  resection = if_else(resection == "yes", 1L, 0L),
+  charlson_score = factor(charlson_score),
+  sexF = as.integer(sex == "female"),
+  age = as.numeric(age)
+) %>%
+select(-days, -sex) %>%
+as.data.frame()
 
 save_data(tumor)
 
@@ -385,18 +398,18 @@ save_data(cost)
 
 # Exclude LEN.T and FRAC to avoid optimistic bias due to future information
 uis = mlr3misc::load_dataset("uis", "quantreg") |>
-  select(
-    -ID, # ID var
-    -Y, # Y: log(TIME)
-    -LEN.T, -FRAC # Length of stay, compliance, not known at baseline
-  ) |>
-  rename(
-    time = TIME,
-    status = CENSOR
-  ) |>
-  mutate(
-    HC = factor(HC), # Categorical, 4 levels
-  )
+select(
+  -ID, # ID var
+  -Y, # Y: log(TIME)
+  -LEN.T, -FRAC # Length of stay, compliance, not known at baseline
+) |>
+rename(
+  time = TIME,
+  status = CENSOR
+) |>
+mutate(
+  HC = factor(HC), # Categorical, 4 levels
+)
 
 save_data(uis)
 
@@ -410,16 +423,16 @@ save_data(uis)
 
 # Year column in class 'date' DDmonYY format for year extraction
 rdata = mlr3misc::load_dataset("rdata", "relsurv") |>
-  rename(status = cens) |>
-  mutate(
-    # Extract 2 digit year from date of diagnosis
-    year = stringr::str_extract(date:::as.character.date(year), "\\d{2}$"),
-    year = factor(year),
-    # recode sex from 1,2 to 0,1 for consistency
-    sex = sex - 1
-  ) |>
-  # Redundant variable, grouped version of age
-  select(-agegr)
+rename(status = cens) |>
+mutate(
+  # Extract 2 digit year from date of diagnosis
+  year = stringr::str_extract(date:::as.character.date(year), "\\d{2}$"),
+  year = factor(year),
+  # recode sex from 1,2 to 0,1 for consistency
+  sex = sex - 1
+) |>
+# Redundant variable, grouped version of age
+select(-agegr)
 
 # Sanity check to ensure year recoding worked
 stopifnot(length(unique(rdata$year)) == 5)
@@ -432,13 +445,13 @@ save_data(rdata)
 # Removed due to date column by ML
 # Re-added with converted date col and further preprocessing
 colrec = relsurv::colrec |>
-  mutate(
-    # Convert date variable to numeric
-    diag = as.numeric(diag),
-    stage = ifelse(stage == 99, NA_real_, stage),
-    sex = factor(sex, levels = 1:2, labels = c("male", "female"))
-  ) |>
-  rename(status = stat)
+mutate(
+  # Convert date variable to numeric
+  diag = as.numeric(diag),
+  stage = ifelse(stage == 99, NA_real_, stage),
+  sex = factor(sex, levels = 1:2, labels = c("male", "female"))
+) |>
+rename(status = stat)
 
 save_data(colrec)
 
@@ -450,17 +463,17 @@ save_data(colrec)
 # based on usage in simPH example code with {survival} etc.
 
 CarpenterFdaData = mlr3misc::load_dataset("CarpenterFdaData", "simPH") |>
-  rename(
-    time = acttime,
-    status = censor
-  ) |>
-  select(
-    -caseid, # ID var
-    -X_st, # constant 1
-    -X_t0, # constant 0
-    -X_t, # identical to acttime (time).
-    -X_d # identical to censor (status)
-  )
+rename(
+  time = acttime,
+  status = censor
+) |>
+select(
+  -caseid, # ID var
+  -X_st, # constant 1
+  -X_t0, # constant 0
+  -X_t, # identical to acttime (time).
+  -X_d # identical to censor (status)
+)
 
 save_data(CarpenterFdaData)
 
@@ -470,84 +483,84 @@ save_data(CarpenterFdaData)
 # cure model (Kirkwood, et al., 1996)
 
 e1684 = mlr3misc::load_dataset("e1684", "smcure") |>
-  rename(time = FAILTIME, status = FAILCENS)
+rename(time = FAILTIME, status = FAILCENS)
 
 save_data(e1684)
 
 # survival::flchain ---------------------------------------------------------------------------
 flchain = mlr3misc::load_dataset("flchain", "survival") %>%
-  mutate(
-    mgus = as.factor(as.character(mgus)),
-    time = futime,
-    flc.grp = as.factor(as.character(flc.grp)),
-    status = death
-  ) %>%
-  select(-futime, -chapter, -death, -creatinine, -chapter) %>%
-  filter(time > 0)
+mutate(
+  mgus = as.factor(as.character(mgus)),
+  time = futime,
+  flc.grp = as.factor(as.character(flc.grp)),
+  status = death
+) %>%
+select(-futime, -chapter, -death, -creatinine, -chapter) %>%
+filter(time > 0)
 
 save_data(flchain)
 
 # survival::lung ------------------------------------------------------------------------------
 lung = mlr3misc::load_dataset("lung", "survival") |>
-  mutate(
-    sex = factor(if_else(sex == 1, "M", "F")),
-    ph.ecog = as.integer(ph.ecog),
-    inst = factor(inst),
-    status = status - 1
-  ) |>
-  tibble::remove_rownames()
+mutate(
+  sex = factor(if_else(sex == 1, "M", "F")),
+  ph.ecog = as.integer(ph.ecog),
+  inst = factor(inst),
+  status = status - 1
+) |>
+tibble::remove_rownames()
 
 save_data(lung)
 
 # survival::nafld1 ----------------------------------------------------------------------------
 nafld1 = survival::nafld1 |>
-  mutate(
-    sex = factor(if_else(male == 0, "F", "M")),
-    # Convert time (days) to whole weeks to reduce number of unique time points
-    time = round(futime / 7)
-  ) |>
-  select(-male, -case.id, -futime, -id)
+mutate(
+  sex = factor(if_else(male == 0, "F", "M")),
+  # Convert time (days) to whole weeks to reduce number of unique time points
+  time = round(futime / 7)
+) |>
+select(-male, -case.id, -futime, -id)
 
 save_data(nafld1)
 
 # survival::nwtco -----------------------------------------------------------------------------
 nwtco = mlr3misc::load_dataset("nwtco", "survival") %>%
-  mutate(
-    status = rel,
-    time = as.numeric(edrel),
-    age = as.numeric(age),
-    histol = as.integer(histol - 1),
-    stage = factor(stage)
-  ) %>%
-  select(-seqno, -study, -rel, -edrel, -in.subcohort, -instit)
+mutate(
+  status = rel,
+  time = as.numeric(edrel),
+  age = as.numeric(age),
+  histol = as.integer(histol - 1),
+  stage = factor(stage)
+) %>%
+select(-seqno, -study, -rel, -edrel, -in.subcohort, -instit)
 
 # survival::transplant ------------------------------------------------------------------------
 transplant = mlr3misc::load_dataset("transplant", "survival") %>%
-  mutate(
-    time = futime,
-    status = if_else(event == "death", 1, 0)
-  ) %>%
-  select(-futime, -event) %>%
-  filter(time > 0)
+mutate(
+  time = futime,
+  status = if_else(event == "death", 1, 0)
+) %>%
+select(-futime, -event) %>%
+filter(time > 0)
 
 save_data(transplant)
 
 # survival::mgus ------------------------------------------------------------------------------
 mgus = mlr3misc::load_dataset("mgus", "survival") %>%
-  mutate(
-    time = futime,
-    status = death
-  ) %>%
-  select(-futime, -death, -pcdx, -id, -pctime)
+mutate(
+  time = futime,
+  status = death
+) %>%
+select(-futime, -death, -pcdx, -id, -pctime)
 
 save_data(mgus)
 
 # survival::veteran ---------------------------------------------------------------------------
 veteran = mlr3misc::load_dataset("veteran", "survival") %>%
-  mutate(
-    trt = as.factor(as.character(trt)),
-    prior = factor(if_else(prior == 0, "0", "1"))
-  )
+mutate(
+  trt = as.factor(as.character(trt)),
+  prior = factor(if_else(prior == 0, "0", "1"))
+)
 
 save_data(veteran)
 
@@ -556,43 +569,43 @@ save_data(veteran)
 if (requireNamespace("reticulate", quietly = TRUE)) {
   # reticulate::py_install("pycox")
   pycox = reticulate::import("pycox")
-
+  
   gbsg = pycox$datasets$gbsg$read_df() %>%
-    mutate(
-      x0 = as.integer(x0),
-      x1 = as.integer(x1),
-      x2 = as.integer(x2),
-      status = event,
-      time = as.numeric(duration)
-    ) %>%
-    select(-event, -duration)
-
+  mutate(
+    x0 = as.integer(x0),
+    x1 = as.integer(x1),
+    x2 = as.integer(x2),
+    status = event,
+    time = as.numeric(duration)
+  ) %>%
+  select(-event, -duration)
+  
   save_data(gbsg)
-
+  
   metabric = pycox$datasets$metabric$read_df() %>%
-    mutate(
-      x4 = as.integer(x4),
-      x5 = as.integer(x5),
-      x6 = as.integer(x6),
-      x7 = as.integer(x7),
-      status = event,
-      time = as.numeric(duration)
-    ) %>%
-    select(-event, -duration) %>%
-    filter(time > 0)
-
+  mutate(
+    x4 = as.integer(x4),
+    x5 = as.integer(x5),
+    x6 = as.integer(x6),
+    x7 = as.integer(x7),
+    status = event,
+    time = as.numeric(duration)
+  ) %>%
+  select(-event, -duration) %>%
+  filter(time > 0)
+  
   save_data(metabric)
-
+  
   support = pycox$datasets$support$read_df() %>%
-    mutate(
-      x1 = as.integer(x1),
-      x4 = as.integer(x4),
-      x5 = as.integer(x5),
-      x6 = as.integer(x6),
-      status = event,
-      time = as.numeric(duration)
-    ) %>%
-    select(-event, -duration)
-
+  mutate(
+    x1 = as.integer(x1),
+    x4 = as.integer(x4),
+    x5 = as.integer(x5),
+    x6 = as.integer(x6),
+    status = event,
+    time = as.numeric(duration)
+  ) %>%
+  select(-event, -duration)
+  
   save_data(support)
 }
